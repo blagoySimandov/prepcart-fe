e94e33f05f2e391c60dc37d878b813fd9f44c432
blagoySimandov <baggy.simandoff@gmail.com>
2025-06-13 00:27:42 +0300

    feat: using env

--- app.config.js
+++ app.config.js
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 export default {
   expo: {
     name: "prepcartv2",
@@ -25,13 +27,13 @@ export default {
       favicon: "./assets/favicon.png",
     },
     extra: {
-      firebaseApiKey: "AIzaSyCv-NV0p8JX2o2qzuyHeTHJhhpfAIXW1xY",
-      firebaseAuthDomain: "prepcart-prod.firebaseapp.com",
-      firebaseProjectId: "prepcart-prod",
-      firebaseStorageBucket: "prepcart-prod.firebasestorage.app",
-      firebaseMessagingSenderId: "60659278182",
-      firebaseAppId: "1:60659278182:web:90b5b603a46cf22a1968a7",
-      firebaseMeasurementId: "G-LGPYPWN70K",
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
     },
   },
 };
