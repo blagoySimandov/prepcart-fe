e5683f076527071c0e89bb35c37d39661706311f
blagoySimandov <baggy.simandoff@gmail.com>
2025-07-16 16:43:33 +0300

    feat: update google sign in font-size

--- app/auth/components/google-sign-button.tsx
+++ app/auth/components/google-sign-button.tsx
@@ -1,5 +1,6 @@
 import { ThemedText } from "@/components/ThemedText";
 import { useThemeColor } from "@/hooks/useThemeColor";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { useStyles } from "../styles";
@@ -23,11 +24,22 @@ export function GoogleSignButton({
         {
           backgroundColor: disabled ? `${tint}80` : tint,
           opacity: disabled ? 0.6 : 1,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 8,
         },
       ]}
       onPress={onPress}
-      disabled={disabled}>
-      <ThemedText type="defaultSemiBold" lightColor="#fff" darkColor="#000">
+      disabled={disabled}
+    >
+      <MaterialIcons name="account-circle" size={20} color="white" />
+      <ThemedText
+        type="defaultSemiBold"
+        lightColor="#fff"
+        darkColor="#000"
+        style={{ fontSize: 18 }}
+      >
         Sign in with Google
       </ThemedText>
     </TouchableOpacity>
